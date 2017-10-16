f57dcf925019ffea1cdcd91a57b1a0df938b7732
Sergey Nesterov <qnesterr@gmail.com>
2017-10-16 13:12:19 +0300

    Commented gem functions

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -1,6 +1,13 @@
 require 'nmax/version'
 
 module Nmax
+  # Find all decimals in stdin
+  # Slice result array to N
+  #
+  # @params stdin - STDIN
+  # @params n [INTEGER] - size of result array
+  #
+  # @return [Array] - array of found numbers
   def find_decimals_in_stdin(stdin, n)
     start_t = Time.now
     puts 'Processing. Please wait...'
@@ -15,6 +22,9 @@ module Nmax
     @numbers.first(n)
   end
 
+  # Find numbers in line and add it to @numbers
+  #
+  # @param line [String] - line for searching
   def line_to_digits(line)
     line.scan(/\d+/) do |digit|
       @numbers << digit.to_i
